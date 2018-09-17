8035c097d441d080a7be893732114b4b29c8908b
Taylor Taake <tt0874535@otc.edu>
2018-09-16 19:34:38 -0700

    random push

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script>
 //Toggle Dropdown between hiding and showing
 function dropDown() {
   document.getElementById("dropdown").classList.toggle("show");
@@ -18,23 +19,24 @@ window.onclick = function(event) {
   }
 }
 
-// Get the modal
-var modal = document.getElementById('theModal');
+  // Get the modal
+  var modal = document.getElementById('theModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('modal');
-var modalImg = document.getElementById("imgOne");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById('modalImage');
+  var modalImg = document.getElementById("modalImage");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+</script>
