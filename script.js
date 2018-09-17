2766cad5bd895b36c2304338175d843d044a693f
Austin <austin8897@gmail.com>
2018-09-16 21:04:45 -0500

    Completed Javascript for Dropdown

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//Toggle Dropdown between hiding and showing
+function myFunction() {
+  document.getElementById("Dropdown").classList.toggle("show");
+}
+
+//Close Dropdown when user clicks away
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbutton')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 // Get the modal
 var modal = document.getElementById('myModal');
 
