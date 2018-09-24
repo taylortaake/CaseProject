0696d5a3f217f4e3dab69959e9ad925f9a5145e8
Austin <austin8897@gmail.com>
2018-09-24 10:34:36 -0500

    Added Array and Loop + images
    Broken, but fixable

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ window.onclick = function(event) {
     }
   }
 }
+
+//Defining array for image spawning
+var parts = new Array("a CPU!", "a GPU!", "RAM!",
+"an SSD!", "a motherboard!", "a powersupply!");
+
+//Function for spawning images from pre-defined array
+function printPictures(){
+  document.write("<h4>Click any picture to find out what it is.</h4>");
+  for (let i = 0; i < parts.length; i++) {
+    document.write("<img src='part" + i + ".jpg' onclick='alert(" + i + ")' id='" + i + "'/>");
+    document.getElementById(i).addEventListener("click", function(){
+      alert("This is "+ parts[i]);
+    }, false);
+  }
+}
