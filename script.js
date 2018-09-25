3ea8dbc69a28dbec903b93fed94c6c4a41579443
Taylor Taake <tt0874535@otc.edu>
2018-09-24 21:09:32 -0700

    added an array for the pictures

--- script.js
+++ script.js
@@ -21,11 +21,16 @@ window.onclick = function(event) {
 //Defining array for image spawning
 var parts = new Array("a CPU!", "a GPU!", "RAM!",
 "an SSD!", "a motherboard!", "a powersupply!");
+var imageArray = new Array["part0.jpg", "part1.jpg", "part2.jpg", "part3.jpg", "part4.jpg", "part5.jpg"]
 
 //Function for spawning images from pre-defined array
 function printPictures(){
   document.write("<h4>Click any picture to find out what it is.</h4>");
+
   for (let i = 0; i < parts.length; i++) {
+    for(let j = 0; j <imageArray.length; j++){
+
+    }
     document.write("<img src='part" + i + ".jpg' onclick='alert(" + i + ")' id='" + i + "'/>");
     document.getElementById(i).addEventListener("click", function(){
       alert("This is "+ parts[i]);
